43e6c2fa41bcca178223a9bbb0650b9dae531211
SigitGunawan <150207612+SigitGunawan@users.noreply.github.com>
2023-12-23 21:32:14 +0700

    upload file pertama saya
    upload file menggunakan github web

---
+++ main.js
@@ -0,0 +1,18 @@
+let menu_btn = document.getElementById("menu_btn")
+let menu_container = document.getElementById("menu_container")
+let showMenu = false
+
+menu_btn.addEventListener("click", ()=>{
+
+    if(showMenu == false){
+        menu_container.style.animation = "slideInd 500ms"
+        menu_container.style.transform = "translateX(0%)"
+
+    } else {
+        menu_container.style.animation = "slideOut 500ms"
+        menu_container.style.transform = "translateX(100%)"
+    }
+
+    showMenu = !showMenu
+
+})
